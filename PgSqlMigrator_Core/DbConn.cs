54854098bb5e96a715febddfce734a82c2febe43
Denis Bondarenko <JellyBall@mail.ru>
2022-09-30 10:47:02 +0700

    Подключение к базе данных

---
+++ PgSqlMigrator_Core/DbConn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace PgSqlMigrator_Core
+{
+    public static class DbConn
+    {
+        public static NpgsqlConnection CreateConn(string address, string login, string password, string dbname)
+        {
+            string connString = $"Host={address};Username={login};Password={password};Database={dbname}";
+            NpgsqlConnection connection = new NpgsqlConnection(connString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Успешно.");
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                return null;
+            }
+        }
+    }
+}
--- PgSqlMigrator_Core/Program.cs
+++ PgSqlMigrator_Core/Program.cs
@@ -4,26 +4,35 @@ namespace PgSqlMigrator_Core
 {
     class Program
     {
-        string inAddress;
-        string inLogin;
-        string inPass;
+        static string inAddress;
+        static string inLogin;
+        static string inPass;
         string outAddress;
         string outLogin;
         string outPass;
         
         static void Main(string[] args)
         {
-            
+            Console.Write("Введите адрес базы данных: ");
+            inAddress = Console.ReadLine();
+            Console.Write("Введите логин базы данных: ");
+            inLogin = Console.ReadLine();
+            Console.Write("Введите пароль базы данных: ");
+            inPass = Console.ReadLine();
+
+            ChangeIn();
         }
 
         private void Registration()
         {
-
+            
         }
 
-        private void ChangeIn()
+        static private void ChangeIn()
         {
-
+            Console.Write("Введите имя базы данных: ");
+            string dbName = Console.ReadLine();
+            DbConn.CreateConn(inAddress, inLogin, inPass, dbName);
         }
 
         private void ChangeOut()
