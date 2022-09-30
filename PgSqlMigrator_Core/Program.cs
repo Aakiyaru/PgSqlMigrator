d8ae5f4b8d63c27da771d4e0be7800173072a37d
Denis Bondarenko <JellyBall@mail.ru>
2022-09-30 09:39:44 +0700

    Добавьте файлы проекта.

---
+++ PgSqlMigrator_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PgSqlMigrator_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
