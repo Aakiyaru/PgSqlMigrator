2609b894aca6b81b56686c4ce3bd5d20fe9675b0
Denis Bondarenko <JellyBall@mail.ru>
2022-10-10 11:45:47 +0700

    загрузка данных с одним полем на сервер

--- PgSqlMigrator_Core/CommandExecutor.cs
+++ PgSqlMigrator_Core/CommandExecutor.cs
@@ -9,14 +9,45 @@ namespace PgSqlMigrator_Core
 {
     public class CommandExecutor
     {
-
+        /// <summary>
+        /// Метод исполнения команды
+        /// </summary>
+        /// <param name="connIn"></param>
+        /// <param name="connOut"></param>
+        /// <param name="table"></param>
+        /// <returns>true либо false в зависимости от успешности операции</returns>
         public static bool Execute(NpgsqlConnection connIn, NpgsqlConnection connOut, string table)
         {
             try
             {
-                NpgsqlCommand command = new NpgsqlCommand($"SELECT * FROM public.\"{table}\";", connOut);
-                NpgsqlDataReader reader = command.ExecuteReader();
+                List<string> downloadData = new List<string>();
+                NpgsqlCommand commandOut = new NpgsqlCommand($"SELECT * FROM public.\"{table}\";", connOut);
+                NpgsqlDataReader reader = commandOut.ExecuteReader();
                 Console.WriteLine($"{DateTime.Now}: Данные с сервера получены.");
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            if (reader.GetFieldType(i).ToString() != "System.String")
+                            {
+                                downloadData.Add(Convert.ToString(reader.GetValue(i)));
+                            }
+                            else
+                            {
+                                downloadData.Add(reader.GetString(i));
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine($"{DateTime.Now}: Данные с сервера собраны в пакет.");
+                
+                for (int i = 0; i < downloadData.Count; i++)
+                {
+                    Console.WriteLine(downloadData[i]);
+                }
 
                 reader.Close();
                 return true;
@@ -28,15 +59,60 @@ namespace PgSqlMigrator_Core
             }
         }
 
+        /// <summary>
+        /// Метод исполнения команды
+        /// </summary>
+        /// <param name="connIn"></param>
+        /// <param name="connOut"></param>
+        /// <param name="table"></param>
+        /// <param name="field"></param>
+        /// <returns>true либо false в зависимости от успешности операции</returns>
         public static bool Execute(NpgsqlConnection connIn, NpgsqlConnection connOut, string table, string field)
         {
             try
             {
-                NpgsqlCommand command = new NpgsqlCommand($"SELECT {field} FROM public.\"{table}\";", connOut);
-                NpgsqlDataReader reader = command.ExecuteReader();
+                List<string> downloadData = new List<string>();
+                NpgsqlCommand commandOut = new NpgsqlCommand($"SELECT {field} FROM public.\"{table}\";", connOut);
+                NpgsqlDataReader reader = commandOut.ExecuteReader();
                 Console.WriteLine($"{DateTime.Now}: Данные с сервера получены.");
 
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            if (reader.GetFieldType(i).ToString() != "System.String")
+                            {
+                                downloadData.Add(Convert.ToString(reader.GetValue(i)));
+                            }
+                            else
+                            {
+                                downloadData.Add(reader.GetString(i));
+                            }
+                        }
+                    }
+                }
                 reader.Close();
+                Console.WriteLine($"{DateTime.Now}: Данные с сервера собраны в пакет.");
+
+                try
+                {
+                    for (int i = 0; i < downloadData.Count; i++)
+                    {
+                        NpgsqlCommand commandIn = new NpgsqlCommand(
+                            $"CREATE TABLE IF NOT EXISTS \"{table}\" ({field} TEXT);" +
+                            $"INSERT INTO \"{table}\"({field}) VALUES ('{downloadData[i]}');", connIn);
+                        commandIn.ExecuteNonQuery();
+                    }
+                    Console.WriteLine($"{DateTime.Now}: Данные отправлены на второй сервер.");
+                    Console.WriteLine($"УСПЕШНО! Операция возобновится через 1 минуту...\n  ");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{DateTime.Now}: {ex.Message}");
+                }
+
                 return true;
             }
             catch (Exception ex)
